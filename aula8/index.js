50e7aeee9e9fe3e08c823f9686bbf181f74423e7
Edmar Piacentini Junior <juniorpiacentini@hotmail.com>
2023-12-07 09:58:19 -0300

    Criadas pastas das aulas 3 e 8

---
+++ aula8/index.js
@@ -0,0 +1,14 @@
+const nome = 'Junior'
+const sobrenome = 'Piacentini'
+const idade = 30
+const peso = 84
+const altura = 1.80
+let imc
+let anoNascimento
+
+imc = peso / (altura * altura)
+anoNascimento = 2023 - idade 
+
+console.log(nome, sobrenome, 'tem', idade, 'anos, e pesa', peso, 'Kg')
+console.log('tem', altura, 'de altura e seu IMC é', imc)
+console.log("nasceu no ano de", anoNascimento)
